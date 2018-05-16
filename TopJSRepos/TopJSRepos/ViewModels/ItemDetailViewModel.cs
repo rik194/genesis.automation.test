ac5235dd0473f30df70734c4471ba2e3ede00816
Riccardo Ravasi <rikr251@gmail.com>
2018-05-16 22:59:11 +0200

    Aggiungere i file di progetto.

---
+++ TopJSRepos/TopJSRepos/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TopJSRepos.Models;
+
+namespace TopJSRepos.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
